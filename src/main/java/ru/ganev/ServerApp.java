c1a9ee3b142722d8aa274793d936517deca3f3a7
Artyom Ganev <a.a.ganev@gmail.com>
2019-09-15 14:27:47 +0300

    Remove CrossOrigin

--- src/main/java/ru/ganev/ServerApp.java
+++ src/main/java/ru/ganev/ServerApp.java
@@ -3,6 +3,8 @@ package ru.ganev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Server Application main class
@@ -15,5 +17,11 @@ public class ServerApp {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ServerApp.class, args);
+    }
+
+    @ResponseBody
+    @RequestMapping("/")
+    String startPage() {
+        return "https://shielded-brushlands-46595.herokuapp.com/actuator/health";
     }
 }
--- src/main/java/ru/ganev/controller/BlogController.java
+++ src/main/java/ru/ganev/controller/BlogController.java
@@ -1,9 +1,6 @@
 package ru.ganev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +8,6 @@ import ru.ganev.model.Blog;
 import ru.ganev.service.IBlogService;
 
 import java.util.List;
-import java.util.UUID;
 
 /**
  * Blog Controller
@@ -19,21 +15,14 @@ import java.util.UUID;
  * @author Artyom Ganev
  */
 @RestController
-@RequestMapping("/blog")
-@CrossOrigin(origins = "https://shielded-brushlands-46595.herokuapp.com", maxAge = 3600)
 public class BlogController {
 
     @Autowired
     IBlogService blogService;
 
     @ResponseBody
+    @RequestMapping("/blog")
     List<Blog> blog() {
         return blogService.getBlogList();
-    }
-
-    @CrossOrigin
-    @GetMapping("/{id}")
-    public Blog retrieve(@PathVariable UUID id) {
-        return blogService.getBlog(id);
     }
 }
