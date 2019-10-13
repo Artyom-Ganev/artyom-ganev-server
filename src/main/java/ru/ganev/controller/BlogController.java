8fc0adea97b2003fee687da0509f2f4903e6653b
Artyom Ganev <a.a.ganev@gmail.com>
2019-10-13 13:36:34 +0300

    add CrossOrigin support

--- src/main/java/ru/ganev/controller/BlogController.java
+++ src/main/java/ru/ganev/controller/BlogController.java
@@ -1,6 +1,9 @@
 package ru.ganev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,7 @@ import ru.ganev.model.Blog;
 import ru.ganev.service.IBlogService;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Blog Controller
@@ -15,14 +19,23 @@ import java.util.List;
  * @author Artyom Ganev
  */
 @RestController
+@RequestMapping("/blog")
 public class BlogController {
 
     @Autowired
     IBlogService blogService;
 
+    @CrossOrigin
     @ResponseBody
-    @RequestMapping("/blog")
+    @RequestMapping("/")
     List<Blog> blog() {
         return blogService.getBlogList();
+    }
+
+    @CrossOrigin
+    @GetMapping("/{id}")
+    @ResponseBody
+    public void getBlog(@PathVariable UUID id) {
+        blogService.getBlog(id);
     }
 }
